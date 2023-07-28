8375656f3d8aa6aa50229e8279f4b5a3283016a0
alessandra-rs <135713931+alessandra-rs@users.noreply.github.com>
2023-07-27 22:27:38 -0300

    atualização

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-var expandir = document.querySelector('expandir')
-var burguer = document.querySelector('burguer')
+function clickMenu() {
+    if (itens.style.display == 'block') {
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }      
+}
 
-expandir.addEventListener('click', function(){
-    menuSide.classList.toggle('expandir')
-})
+// var menuItem = document.querySelectorAll('itens')
+
+// function selectLink() {
+//     menuItem.forEach((item)=>
+//         item.classList.remove('ativo')
+//     )
+//     this.classList.add('ativo')
+// }
+
+// menuItem.forEach((item)=>
+//     item.addEventListener('click', selectLink)
+// )
+
+// var expandir = document.querySelector('expandir')
+// var burguer = document.querySelector('burguer')
+
+// expandir.addEventListener('click', function(){
+//     menuSide.classList.toggle('expandir')
+// })
