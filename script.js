9a66dd17472900b63386ab52a4c5144a386b7721
alessandra-rs <135713931+alessandra-rs@users.noreply.github.com>
2023-07-27 21:43:12 -0300

    protótipo Medfarm

---
+++ script.js
@@ -0,0 +1,6 @@
+var expandir = document.querySelector('expandir')
+var burguer = document.querySelector('burguer')
+
+expandir.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
